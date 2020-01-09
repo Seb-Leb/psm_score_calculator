351a61ec7d936e9091316f320d1fb32f23978df6
Seb-Leb <seb@leb.cat>
2020-01-09 11:14:14 -0500

    fixes

--- psm_score.py
+++ psm_score.py
@@ -1,10 +1,13 @@
 import argparse
+import yaml
+import random
+import itertools as itt
 import numpy as np
 
-parser = argparse.ArgumentParser(description='Evaluate PSM score from PeptideShaker reports.')
-parser.add_argument('file_paths', metavar='file_paths', type=str, nargs='+',
-                    help='space separated list of absolute filepaths to the PeptideShaker reports.')
-args = parser.parse_args()
+#parser = argparse.ArgumentParser(description='Evaluate PSM score from PeptideShaker reports.')
+#parser.add_argument('file_paths', metavar='file_paths', type=str, nargs='+',
+                    #help='space separated list of absolute filepaths to the PeptideShaker reports.')
+#args = parser.parse_args()
 
 
 class Spectrum:
@@ -53,11 +56,36 @@ class Score:
 
         pass
 
+    def get_random_peptides(self, pep_seq, n_random=10000):
+        pep_aas = list(pep_seq)
+        rand_pep_seqs = set()
+        if len(pep_seq) < 8 or len(set(pep_seq)) < 7:
+            return list(set([''.join(x) for x in itt.permutations(pep_seq)]))
+        while len(rand_pep_seqs)<n_random:
+            random.shuffle(pep_aas)
+            rand_pep = ''.join(pep_aas)
+            if rand_pep != pep_seq:
+                rand_pep_seqs.add(rand_pep)
+        return rand_pep_seqs
 
-if __name__ == "__main__":
-    for fpath in list(args.file_paths):
-        spectrum = Spectrum(fpath)
-        score = Score()
-        annotated_spec = spectrum.parse_report()
-        hscore = score.hyperscore(spectrum.pep_seq, annotated_spec)
-        print('peptide: {}  hyperscore: {}'.format(spectrum.pep_seq, hscore))
+class TheoreticalSpectrum:
+    def __init__(self, pep_seq):
+        pep_len = len(pep_seq)
+        with open('psm_score_calculator/aa_weight.yml', 'r') as f:
+            self.aa_weights = yaml.load(f)
+
+        b_ions, y_ions = np.zeros(pep_len), np.zeros(pep_len)
+        for i in range(pep_len):
+            b_ions[i]           = sum(self.aa_weights[aa] for aa in pep_seq[:i+1])
+            y_ions[pep_len-1-i] = sum(self.aa_weights[aa] for aa in pep_seq[i:])
+
+        self.b_ions = b_ions
+        self.y_ions = y_ions
+
+#if __name__ == "__main__":
+    #for fpath in list(args.file_paths):
+        #spectrum = Spectrum(fpath)
+        #score = Score()
+        #annotated_spec = spectrum.parse_report()
+        #hscore = score.hyperscore(spectrum.pep_seq, annotated_spec)
+        #print('peptide: {}  hyperscore: {}'.format(spectrum.pep_seq, hscore))
