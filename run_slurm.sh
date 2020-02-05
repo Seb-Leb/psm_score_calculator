f9ff0a0d4c84b9f1a066057a87dc757176d2fc4a
Sebastien Leblanc <sleblanc@beluga1.int.ets1.calculquebec.ca>
2020-02-05 13:42:13 -0500

    updated sh

--- run_slurm.sh
+++ run_slurm.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #SBATCH --account=def-xroucou
-#SBATCH --time=07:00:00
-#SBATCH --cpus-per-task=24
-#SBATCH --mem-per-cpu=500M
+#SBATCH --time=10:00:00
+#SBATCH --cpus-per-task=40
+#SBATCH --mem-per-cpu=2G
 
-python3 /nfs3_ib/ip32-ib/home/sleblanc/psm_score_calculator/psm_score_multiProc.py /nfs3_ib/ip32-ib/home/xroucou_group/echange_de_fichiers/Annotation/Annotation_OpenProtAll_9x_3_exp1.txt --output_path /nfs3_ib/ip32-ib/home/xroucou_group/echange_de_fichiers/Annotation/Annotation_OpenProtAll_9x_3_exp1_scores.tsv --n_cpu 24 --n_random 10000
+python3 /home/sleblanc/psm_score_calculator/psm_score_multiProc_pickle.py /home/sleblanc/psms_to_score.pkl --partial_report /home/sleblanc/scored_psms.tsv --n_cpu 40
