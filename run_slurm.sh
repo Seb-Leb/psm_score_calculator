d7ba07832688f5121fc69741dbc79836bccf99a1
bioinfo_seb <sleblanc17@ubishops.ca>
2020-02-09 11:30:45 -0500

    new score run

--- run_slurm.sh
+++ run_slurm.sh
@@ -5,4 +5,4 @@
 #SBATCH --cpus-per-task=40
 #SBATCH --mem-per-cpu=2G
 
-python3 /home/sleblanc/psm_score_calculator/psm_score_multiProc_pickle.py /home/sleblanc/psms_to_score.pkl --partial_report /home/sleblanc/scored_psms.tsv --n_cpu 40
+python3 /home/sleblanc/psm_score_calculator/psm_score_multiProc_pickle.py /home/sleblanc/scores_Classic_Refs.pkl --output_path /home/sleblanc/Classic_Refs_scored_psms.tsv --n_cpu 40 --db Ref
